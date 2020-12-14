eaa5d83fc4e9c02e3e6c9330065c92d40a8fc3b7
Christoph 'knurd' Morrison <github@christoph-morrison.de>
2020-12-14 13:47:41 +0100

    Update der DB-Struktur

--- data/mariadb/structure.sql
+++ data/mariadb/structure.sql
@@ -1,24 +1,21 @@
--- Adminer 4.7.7 MySQL dump
+-- Adminer 4.7.8 MySQL dump
 
-SET NAMES utf8mb4;
+SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-CREATE USER IF NOT EXISTS `abfall` IDENTIFIED BY 'abfall';
-GRANT ALL ON abfall.* TO 'abfall'@'%';
-
-CREATE DATABASE IF NOT EXISTS `abfall`;
-USE `abfall`;
+SET NAMES utf8mb4;
 
 DROP TABLE IF EXISTS `appointments`;
 CREATE TABLE `appointments` (
-    `uuid` varchar(36) COLLATE utf8mb4_german2_ci NOT NULL,
-    `datetime` datetime NOT NULL,
-    `street_id` int(10) unsigned NOT NULL,
-    `collection_id` tinyint(3) unsigned NOT NULL,
-    PRIMARY KEY (`datetime`,`street_id`,`collection_id`)
+  `uuid` varchar(36) COLLATE utf8mb4_german2_ci NOT NULL,
+  `datetime` datetime NOT NULL,
+  `street_id` int(10) unsigned NOT NULL,
+  `collection_id` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY (`datetime`,`street_id`,`collection_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
+
 
 DROP TABLE IF EXISTS `cities`;
 CREATE TABLE `cities` (
@@ -27,8 +24,6 @@ CREATE TABLE `cities` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
 
-INSERT INTO `cities` (`id`, `city`) VALUES
-(445739,	'Gütersloh');
 
 DROP TABLE IF EXISTS `collections`;
 CREATE TABLE `collections` (
@@ -37,13 +32,6 @@ CREATE TABLE `collections` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
 
-INSERT INTO `collections` (`id`, `name`) VALUES
-(1,	'Restmüll 14-tgl.'),
-(2,	'Restmüll 4-wö.'),
-(3,	'Kompost'),
-(4,	'Altpapier'),
-(5,	'Gelber Sack'),
-(6,	'Saisonkompost');
 
 DROP TABLE IF EXISTS `streets`;
 CREATE TABLE `streets` (
@@ -54,4 +42,4 @@ CREATE TABLE `streets` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
 
 
--- 2020-12-08 10:15:47
+-- 2020-12-14 12:46:38
